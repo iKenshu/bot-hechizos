6321c6bee26b90f954d577de699722e616a6da95
Kevin <kenshumorales@gmail.com>
2018-05-29 12:19:55 -0400

    message format and main function

--- bot.py
+++ bot.py
@@ -1,17 +1,12 @@
 import os
 import logging
 import requests
-import telegram
 
+from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-
-
-bot_spells = telegram.Bot(token=os.environ['TOKEN'])
-updater = Updater(bot_spells.token)
-dispatcher = updater.dispatcher
 
 
 def get_spell(spell):
@@ -19,7 +14,10 @@ def get_spell(spell):
 
     get_spell = requests.get(url)
     spells = get_spell.json()
-    return spells['description']
+    message = f"""Es *{spells['type']}* y *{spells['object']}*. *({spells['range'][0]['name']})*:
+                \n{spells['description']}
+                """
+    return message
 
 
 def start(bot, update):
@@ -35,14 +33,24 @@ def spell(bot, update):
     Respond with the /hechizo command
     """
     message = update.message.text.split(' ')
+    print(len(message))
     spell = message[1]
-    bot.send_message(chat_id=update.message.chat_id,
-                     text=get_spell(spell))
+    update.message.reply_text(text=get_spell(spell),
+                              parse_mode=ParseMode.MARKDOWN)
 
 
-start_handler = CommandHandler('start', start)
-spell_handler = CommandHandler('hechizo', spell)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(spell_handler)
+def main():
+    # update_spells = telegram.Bot()
+    updater = Updater(token=os.environ['TOKEN'])
+    dispatcher = updater.dispatcher
 
-updater.start_polling()
+    start_handler = CommandHandler('start', start)
+    spell_handler = CommandHandler('hechizo', spell)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(spell_handler)
+
+    updater.start_polling()
+
+
+if __name__ == '__main__':
+    main()

0d7d46a05be2badef6b670fa93d436738766bab2
Kevin <kenshumorales@gmail.com>
2018-05-30 11:04:01 -0400

    deploy to heroku

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import os
 import logging
 import requests
 
+from slugify import slugify
 from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
 
 
 def get_spell(spell):
@@ -17,6 +17,19 @@ def get_spell(spell):
     message = f"""(*{spells['range'][0]['name']}*) Es *{spells['type']}* y *{spells['object']}*.:
                 \n{spells['description']}
                 """
+    return message
+
+
+def get_range(range):
+    url = 'http://hechizos.herokuapp.com/api/range/' + range
+
+    get_spell = requests.get(url)
+    ranges = get_spell.json()
+    spell_name = []
+    for spell in ranges:
+        spell_name.append(spell['name'])
+    message_format = ', '.join(spell_name)
+    message = f'{message_format}'
     return message
 
 
@@ -32,22 +45,48 @@ def spell(bot, update):
     """
     Respond with the /hechizo command
     """
-    message = update.message.text.split(' ')
-    print(len(message))
-    spell = message[1]
-    update.message.reply_text(text=get_spell(spell),
+    message = slugify(update.message.text)
+    spell = message[8:]
+    try:
+        update.message.reply_text(text=get_spell(spell),
+                                  parse_mode=ParseMode.MARKDOWN)
+    finally:
+        update.message.reply_text(text='Creo que estás haciendo algo mal')
+
+
+def range(bot, update):
+    """
+    Respond with the /range command
+    """
+    message = slugify(update.message.text)
+    range = message[6:]
+    update.message.reply_text(text=get_range(range),
                               parse_mode=ParseMode.MARKDOWN)
 
 
 def main():
-    # update_spells = telegram.Bot()
-    updater = Updater(token=os.environ['TOKEN'])
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+    NAME = 'hechizoshlbot'
+    PORT = os.environ.get('PORT')
+    TOKEN = os.environ.get('TOKEN')
+
+    updater = Updater(token=TOKEN)
+
     dispatcher = updater.dispatcher
 
     start_handler = CommandHandler('start', start)
     spell_handler = CommandHandler('hechizo', spell)
+    range_handler = CommandHandler('rango', range)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(spell_handler)
+    dispatcher.add_handler(range_handler)
+
+    updater.start_webhook(listen=0.0.0.0,
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook(f'https://{NAME}.herokuapp.com/{TOKEN}')
 
     updater.start_polling()
 

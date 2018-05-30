a5806b83eec7cbddd0fd064580f2633b049a39bc
Kevin <kenshumorales@gmail.com>
2018-05-30 12:33:39 -0400

    formating message

--- bot.py
+++ bot.py
@@ -16,7 +16,8 @@ def get_spell(spell):
     get_spell = requests.get(url)
     spells = get_spell.json()
     message = f"""(*{spells['range'][0]['name']}*) Es *{spells['type']}* y *{spells['method']}*.:
-                \n{spells['description']}\n{spells['object']}"""
+                \n{spells['description']}
+                \n{spells['object']}"""
     return message
 
 
